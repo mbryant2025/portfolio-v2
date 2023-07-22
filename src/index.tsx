830fd6f5e0335b2f2de75c4b9ef1093a87bd56fa
Michael Bryant <mcb115@duke.edu>
2023-07-22 15:48:22 -0400

    force updates

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ root.render(
       <App />
     </HashRouter>
   </React.StrictMode>
-);
+); 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
