9f7022694249cf9895dd129b248285e878019857
Michael Bryant <mcb115@duke.edu>
2023-07-22 14:22:40 -0400

    temp remove snake

--- src/pages/games/SnakePlusPlus.tsx
+++ src/pages/games/SnakePlusPlus.tsx
@@ -1,235 +1,237 @@
-import React, { useState, useEffect, useCallback } from "react";
-import BackButton from "../../components/BackButton";
+// import React, { useState, useEffect, useCallback } from "react";
+// import BackButton from "../../components/BackButton";
 import "../../components/styles/snake.css";
 
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right
-}
+import React from 'react';
 
-interface SnakeSegment {
-    x: number;
-    y: number;
-}
+// enum Direction {
+//     Up,
+//     Down,
+//     Left,
+//     Right
+// }
 
-const initialSnake: SnakeSegment[] = [
-    { x: 10, y: 10 },
-    { x: 10, y: 11 }
-];
+// interface SnakeSegment {
+//     x: number;
+//     y: number;
+// }
 
-const intialEnemySnake: SnakeSegment[] = [
-    { x: 5, y: 10 },
-    { x: 5, y: 11 },
-    { x: 5, y: 12 },
-    { x: 5, y: 13 }
-];
+// const initialSnake: SnakeSegment[] = [
+//     { x: 10, y: 10 },
+//     { x: 10, y: 11 }
+// ];
 
-const rows = 20;
-const columns = 30;
-const enemySpeed = 50;
-const enemyKillableDuration = 3000; // ms
+// const intialEnemySnake: SnakeSegment[] = [
+//     { x: 5, y: 10 },
+//     { x: 5, y: 11 },
+//     { x: 5, y: 12 },
+//     { x: 5, y: 13 }
+// ];
+
+// const rows = 20;
+// const columns = 30;
+// const enemySpeed = 50;
+// const enemyKillableDuration = 3000; // ms
 
 const SnakePlusPlus: React.FC = () => {
-    const [snake, setSnake] = useState<SnakeSegment[]>(initialSnake);
-    const [direction, setDirection] = useState(Direction.Right);
-    const [apple, setApple] = useState(generateRandomApplePosition());
-    const [startGame, setStartGame] = useState(false);
-    const [score, setScore] = useState<number>(0);
-    const [desiredLength, setDesiredLength] = useState<number>(2);
-    const [enemySnake, setEnemySnake] = useState<SnakeSegment[]>(intialEnemySnake);
-    const [enemyKillable, setEnemyKillable] = useState<boolean>(false);
+    // const [snake, setSnake] = useState<SnakeSegment[]>(initialSnake);
+    // const [direction, setDirection] = useState(Direction.Right);
+    // const [apple, setApple] = useState(generateRandomApplePosition());
+    // const [startGame, setStartGame] = useState(false);
+    // const [score, setScore] = useState<number>(0);
+    // const [desiredLength, setDesiredLength] = useState<number>(2);
+    // const [enemySnake, setEnemySnake] = useState<SnakeSegment[]>(intialEnemySnake);
+    // const [enemyKillable, setEnemyKillable] = useState<boolean>(false);
 
-    function generateRandomApplePosition() {
-        // If the snake is as long as there are cells on the board, the game is won
-        if (snake.length === rows * columns) {
-            return { x: -1, y: -1 };
-        }
-        let newApple: SnakeSegment;
-        do {
-            newApple = {
-                x: Math.floor(Math.random() * columns),
-                y: Math.floor(Math.random() * rows)
-            };
-        } while (
-            snake.some(
-                segment => segment.x === newApple.x && segment.y === newApple.y
-            )
-        );
-        return newApple;
-    }
+    // function generateRandomApplePosition() {
+    //     // If the snake is as long as there are cells on the board, the game is won
+    //     if (snake.length === rows * columns) {
+    //         return { x: -1, y: -1 };
+    //     }
+    //     let newApple: SnakeSegment;
+    //     do {
+    //         newApple = {
+    //             x: Math.floor(Math.random() * columns),
+    //             y: Math.floor(Math.random() * rows)
+    //         };
+    //     } while (
+    //         snake.some(
+    //             segment => segment.x === newApple.x && segment.y === newApple.y
+    //         )
+    //     );
+    //     return newApple;
+    // }
 
-    const handleStartGame = () => {
-        setScore(2);
-        setDesiredLength(2);
-        setEnemyKillable(false);
-        setSnake(initialSnake);
-        setEnemySnake(intialEnemySnake);
-        setDirection(Direction.Right);
-        setApple(generateRandomApplePosition());
-        setStartGame(true);
-    };
+    // const handleStartGame = () => {
+    //     setScore(2);
+    //     setDesiredLength(2);
+    //     setEnemyKillable(false);
+    //     setSnake(initialSnake);
+    //     setEnemySnake(intialEnemySnake);
+    //     setDirection(Direction.Right);
+    //     setApple(generateRandomApplePosition());
+    //     setStartGame(true);
+    // };
 
-    const moveSnake = useCallback(() => {
-        const head = { ...snake[0] };
+    // const moveSnake = useCallback(() => {
+    //     const head = { ...snake[0] };
 
-        switch (direction) {
-            case Direction.Up:
-                head.y -= 1;
-                break;
-            case Direction.Down:
-                head.y += 1;
-                break;
-            case Direction.Left:
-                head.x -= 1;
-                break;
-            case Direction.Right:
-                head.x += 1;
-                break;
-        }
+    //     switch (direction) {
+    //         case Direction.Up:
+    //             head.y -= 1;
+    //             break;
+    //         case Direction.Down:
+    //             head.y += 1;
+    //             break;
+    //         case Direction.Left:
+    //             head.x -= 1;
+    //             break;
+    //         case Direction.Right:
+    //             head.x += 1;
+    //             break;
+    //     }
 
-        const hitEnemySnake = snake.some(
-            segment =>
-                enemySnake.some(
-                    enemySegment =>
-                        enemySegment.x === segment.x && enemySegment.y === segment.y
-                )
-        )
+    //     const hitEnemySnake = snake.some(
+    //         segment =>
+    //             enemySnake.some(
+    //                 enemySegment =>
+    //                     enemySegment.x === segment.x && enemySegment.y === segment.y
+    //             )
+    //     )
 
-        if (
-            head.x < 0 ||
-            head.x >= columns ||
-            head.y < 0 ||
-            head.y >= rows ||
-            snake.some(segment => segment.x === head.x && segment.y === head.y) ||
-            (!enemyKillable && hitEnemySnake)
-        ) {
-            // Snake hits the edge or itself, reset the game
-            setStartGame(false);
-            return;
-        }
+    //     if (
+    //         head.x < 0 ||
+    //         head.x >= columns ||
+    //         head.y < 0 ||
+    //         head.y >= rows ||
+    //         snake.some(segment => segment.x === head.x && segment.y === head.y) ||
+    //         (!enemyKillable && hitEnemySnake)
+    //     ) {
+    //         // Snake hits the edge or itself, reset the game
+    //         setStartGame(false);
+    //         return;
+    //     }
 
-        // If the snakes collide and the enemy is killable, remove the last enemy segment
-        if (hitEnemySnake && enemyKillable) {
-            setEnemySnake(enemySnake.slice(0, -1));
-        }
+    //     // If the snakes collide and the enemy is killable, remove the last enemy segment
+    //     if (hitEnemySnake && enemyKillable) {
+    //         setEnemySnake(enemySnake.slice(0, -1));
+    //     }
 
-        const newSnake = [head, ...snake];
+    //     const newSnake = [head, ...snake];
 
-        if (head.x === apple.x && head.y === apple.y) {
-            // Snake eats the apple, generate a new apple and double the score
-            setApple(generateRandomApplePosition());
-            setDesiredLength(prevLength => prevLength * 2); // Double the desired length
-            // Set enemy as killable
-            setEnemyKillable(true);
-            setTimeout(() => {
-                // Technically problematic if a new game is started before the timeout, but it's fine for now
-                setEnemyKillable(false);
-            }, enemyKillableDuration);
-        } else if (newSnake.length > desiredLength) {
-            // Remove the last segment of the snake if it exceeds the desired length
-            newSnake.pop();
-        }
+    //     if (head.x === apple.x && head.y === apple.y) {
+    //         // Snake eats the apple, generate a new apple and double the score
+    //         setApple(generateRandomApplePosition());
+    //         setDesiredLength(prevLength => prevLength * 2); // Double the desired length
+    //         // Set enemy as killable
+    //         setEnemyKillable(true);
+    //         setTimeout(() => {
+    //             // Technically problematic if a new game is started before the timeout, but it's fine for now
+    //             setEnemyKillable(false);
+    //         }, enemyKillableDuration);
+    //     } else if (newSnake.length > desiredLength) {
+    //         // Remove the last segment of the snake if it exceeds the desired length
+    //         newSnake.pop();
+    //     }
 
-        setScore(newSnake.length);
+    //     setScore(newSnake.length);
 
-        setSnake(newSnake);
-    }, [snake, direction, apple, desiredLength, score]);
+    //     setSnake(newSnake);
+    // }, [snake, direction, apple, desiredLength, score]);
 
-    const moveEnemySnake = useCallback(() => {
+    // const moveEnemySnake = useCallback(() => {
 
-        // If the enemy is length 0 (dead), return
-        if (enemySnake.length === 0) {
-            return;
-        }
+    //     // If the enemy is length 0 (dead), return
+    //     if (enemySnake.length === 0) {
+    //         return;
+    //     }
 
-        const enemyHead = { ...enemySnake[0] };
-        const playerHead = { ...snake[0] };
+    //     const enemyHead = { ...enemySnake[0] };
+    //     const playerHead = { ...snake[0] };
 
-        if (!enemyKillable) {
+    //     if (!enemyKillable) {
 
-            if (playerHead.x < enemyHead.x) {
-                enemyHead.x -= 1;
-            } else if (playerHead.x > enemyHead.x) {
-                enemyHead.x += 1;
-            } else if (playerHead.y < enemyHead.y) {
-                enemyHead.y -= 1;
-            } else if (playerHead.y > enemyHead.y) {
-                enemyHead.y += 1;
-            }
-        }
+    //         if (playerHead.x < enemyHead.x) {
+    //             enemyHead.x -= 1;
+    //         } else if (playerHead.x > enemyHead.x) {
+    //             enemyHead.x += 1;
+    //         } else if (playerHead.y < enemyHead.y) {
+    //             enemyHead.y -= 1;
+    //         } else if (playerHead.y > enemyHead.y) {
+    //             enemyHead.y += 1;
+    //         }
+    //     }
 
-        else {
-            // If killable, move in the opposite direction -- that is, away from the player
+    //     else {
+    //         // If killable, move in the opposite direction -- that is, away from the player
 
-            if (playerHead.x < enemyHead.x) {
-                enemyHead.x += 1;
-            }
-            else if (playerHead.x > enemyHead.x) {
-                enemyHead.x -= 1;
-            }
-            else if (playerHead.y < enemyHead.y) {
-                enemyHead.y += 1;
-            }
-            else if (playerHead.y > enemyHead.y) {
-                enemyHead.y -= 1;
-            }
+    //         if (playerHead.x < enemyHead.x) {
+    //             enemyHead.x += 1;
+    //         }
+    //         else if (playerHead.x > enemyHead.x) {
+    //             enemyHead.x -= 1;
+    //         }
+    //         else if (playerHead.y < enemyHead.y) {
+    //             enemyHead.y += 1;
+    //         }
+    //         else if (playerHead.y > enemyHead.y) {
+    //             enemyHead.y -= 1;
+    //         }
 
-        }
+    //     }
 
-        const newEnemySnake = [enemyHead, ...enemySnake.slice(0, -1)];
+    //     const newEnemySnake = [enemyHead, ...enemySnake.slice(0, -1)];
 
-        setEnemySnake(newEnemySnake);
+    //     setEnemySnake(newEnemySnake);
 
 
-    }, [enemySnake, snake]);
+    // }, [enemySnake, snake]);
 
-    useEffect(() => {
-        const handleKeyDown = (event: KeyboardEvent) => {
-            if (!startGame && event.key === " ") {
-                handleStartGame();
-            }
+    // useEffect(() => {
+    //     const handleKeyDown = (event: KeyboardEvent) => {
+    //         if (!startGame && event.key === " ") {
+    //             handleStartGame();
+    //         }
 
-            switch (event.key) {
-                case "ArrowUp":
-                    setDirection(Direction.Up);
-                    break;
-                case "ArrowDown":
-                    setDirection(Direction.Down);
-                    break;
-                case "ArrowLeft":
-                    setDirection(Direction.Left);
-                    break;
-                case "ArrowRight":
-                    setDirection(Direction.Right);
-                    break;
-            }
-        };
+    //         switch (event.key) {
+    //             case "ArrowUp":
+    //                 setDirection(Direction.Up);
+    //                 break;
+    //             case "ArrowDown":
+    //                 setDirection(Direction.Down);
+    //                 break;
+    //             case "ArrowLeft":
+    //                 setDirection(Direction.Left);
+    //                 break;
+    //             case "ArrowRight":
+    //                 setDirection(Direction.Right);
+    //                 break;
+    //         }
+    //     };
 
-        document.body.addEventListener("keydown", handleKeyDown);
+    //     document.body.addEventListener("keydown", handleKeyDown);
 
-        if (startGame) {
-            const interval = setInterval(moveSnake, 50);
-            return () => {
-                document.body.removeEventListener("keydown", handleKeyDown);
-                clearInterval(interval);
-            };
-        }
-    }, [startGame, moveSnake]);
+    //     if (startGame) {
+    //         const interval = setInterval(moveSnake, 50);
+    //         return () => {
+    //             document.body.removeEventListener("keydown", handleKeyDown);
+    //             clearInterval(interval);
+    //         };
+    //     }
+    // }, [startGame, moveSnake]);
 
-    useEffect(() => {
-        if (startGame) {
-            const enemyInterval = setInterval(moveEnemySnake, enemySpeed);
-            return () => {
-                clearInterval(enemyInterval);
-            };
-        }
-    }, [startGame, moveEnemySnake]);
+    // useEffect(() => {
+    //     if (startGame) {
+    //         const enemyInterval = setInterval(moveEnemySnake, enemySpeed);
+    //         return () => {
+    //             clearInterval(enemyInterval);
+    //         };
+    //     }
+    // }, [startGame, moveEnemySnake]);
 
     return (
         <div>
-            <BackButton />
+            {/* <BackButton />
             <div className="App">
                 <h1 className="title">Snake++</h1>
                 <div className="score">{`Score: ${score}`}</div>
@@ -261,7 +263,7 @@ const SnakePlusPlus: React.FC = () => {
                         Start
                     </button>
                 )}
-            </div>
+            </div> */}
         </div>
     );
 };
