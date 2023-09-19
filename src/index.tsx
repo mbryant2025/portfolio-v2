1e62e1e237150670bfa3a72b76e1651b0fd57dc0
Michael Bryant <mcb115@duke.edu>
2023-09-19 14:39:49 -0400

    i pushed too quickly

--- src/index.tsx
+++ src/index.tsx
@@ -26,7 +26,7 @@ window.addEventListener('resize', () => {
 
   debounceTimeout = setTimeout(() => {
     window.location.reload();
-  }, 50);
+  }, 50); // 50ms is a good debounce time
 });
 
 // If you want to start measuring performance in your app, pass a function
