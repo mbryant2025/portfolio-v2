836bb7865988d57ff8f163e8ca89023523f32d3b
Michael Bryant <mcb115@duke.edu>
2024-05-02 13:00:23 -0400

    linted navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import '../components/styles/navbar.css';
-import { start } from 'repl';
 
 interface NavbarProps {
     selected: string;
@@ -9,36 +8,6 @@ interface NavbarProps {
 }
 
 const Navbar: React.FC<NavbarProps> = ({ selected, animate }) => {
-
-    const handleScroll = () => {
-        const navbar = document.querySelector('.navbar') as HTMLElement;
-        const navbarBackground = document.querySelector('.navbar-background') as HTMLElement;
-        if (navbar) {
-            navbar.style.transition = 'opacity 0.5s';
-
-            const transitionDistance = 400;
-            const opacity = Math.min(Math.max(0,window.scrollY / transitionDistance - 0.85), 1);
-            navbar.style.opacity = `${opacity}`;
-
-            if (window.scrollY === 0) {
-                navbar.style.backgroundColor = 'transparent';
-                navbar.style.opacity = '1';
-            }
-
-            if (window.scrollY > transitionDistance) {
-                navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
-            } else {
-                navbar.style.backgroundColor = 'transparent';
-            }
-        }
-    };
-
-    // useEffect(() => {
-    //     if (animate) {
-    //         window.addEventListener('scroll', handleScroll);
-    //         return () => window.removeEventListener('scroll', handleScroll);
-    //     }
-    // }, [animate]);
 
     return (
         <nav className="navbar">
